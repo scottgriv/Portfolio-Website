41f8a9d8ebb9e261bd19508cb9f5e1eecdb1c44a
Scott Griv <scottgrivner@gmail.com>
2023-08-21 21:24:02 -0400

    Updated title

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -61,7 +61,7 @@ const Hero = () => {
 
   const one = <h1>Hi, my name is</h1>;
   const two = <h2 className="big-heading">Scott Grivner.</h2>;
-  const three = <h3 className="medium-heading">I connect business with technology.</h3>;
+  const three = <h3 className="medium-heading">I connect people and business with technology.</h3>;
   const four = (
     <>
       <p>
