81e76d59413ffab55bc9fd31a951492efdb7dc03
Scott Griv <scottgrivner@gmail.com>
2023-11-08 15:32:02 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,8 @@ module.exports = {
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
-        display: 'minimal-ui',
+        // display: 'minimal-ui',
+        display: `standalone`,
         icon: 'src/images/logo.png',
         icon_options: {
           // For all the options available,
