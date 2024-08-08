f160650ec09530cbba56ee5911de1a136bce164a
Scott Griv <scottgrivner@gmail.com>
2024-08-08 17:30:29 -0400

    Update articles

--- src/components/sections/articles.js
+++ src/components/sections/articles.js
@@ -12,46 +12,6 @@ const StyledArticlesSection = styled.section`
   @media (max-width: 768px) {
     margin: 0 auto 50px;
   }
-
-  .overline {
-    display: block;
-    margin-bottom: 20px;
-    color: var(--green);
-    font-family: var(--font-mono);
-    font-size: var(--fz-md);
-    font-weight: 400;
-
-    &:before {
-      bottom: 0;
-      font-size: var(--fz-sm);
-    }
-
-    &:after {
-      display: none;
-    }
-  }
-
-  .title {
-    font-size: clamp(40px, 5vw, 60px);
-  }
-
-  .email-link {
-    ${({ theme }) => theme.mixins.bigButton};
-    margin-top: 50px;
-  }
-
-  a {
-    text-decoration: none;
-
-    &:hover {
-      text-decoration: none;
-    }
-  }
-
-  .article-image {
-    width: 200px; /* Set your desired static width */
-    margin: 10px;
-  }
 `;
 
 const Articles = () => {
@@ -64,7 +24,7 @@ const Articles = () => {
     }
 
     sr.reveal(revealContainer.current, srConfig());
-  }, [prefersReducedMotion]);
+  }, []);
 
   return (
     <StyledArticlesSection id="articles" ref={revealContainer}>
@@ -74,9 +34,8 @@ const Articles = () => {
         <a
           target="_blank"
           rel="noopener noreferrer"
-          href="https://medium.com/@scottgrivner/understanding-software-ilities-key-attributes-of-quality-software-d4b7fadc9bbe">
+          href="https://github-readme-medium-recent-article.vercel.app/medium/@scottgrivner/0">
           <img
-            className="article-image"
             src="https://github-readme-medium-recent-article.vercel.app/medium/@scottgrivner/0"
             alt="Recent Article 0"
           />
@@ -86,7 +45,6 @@ const Articles = () => {
           rel="noopener noreferrer"
           href="https://github-readme-medium-recent-article.vercel.app/medium/@scottgrivner/1">
           <img
-            className="article-image"
             src="https://github-readme-medium-recent-article.vercel.app/medium/@scottgrivner/1"
             alt="Recent Article 1"
           />
@@ -96,7 +54,6 @@ const Articles = () => {
           rel="noopener noreferrer"
           href="https://github-readme-medium-recent-article.vercel.app/medium/@scottgrivner/2">
           <img
-            className="article-image"
             src="https://github-readme-medium-recent-article.vercel.app/medium/@scottgrivner/2"
             alt="Recent Article 2"
           />
@@ -106,7 +63,6 @@ const Articles = () => {
           rel="noopener noreferrer"
           href="https://github-readme-medium-recent-article.vercel.app/medium/@scottgrivner/3">
           <img
-            className="article-image"
             src="https://github-readme-medium-recent-article.vercel.app/medium/@scottgrivner/3"
             alt="Recent Article 3"
           />
