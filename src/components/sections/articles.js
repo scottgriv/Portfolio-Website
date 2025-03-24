73de2b3a2de495e7bbf2f1a044ce76a0c8be52fe
Scott Griv <scottgrivner@gmail.com>
2025-03-23 21:13:16 -0400

    Update articles

--- src/components/sections/articles.js
+++ src/components/sections/articles.js
@@ -14,6 +14,18 @@ const StyledArticlesSection = styled.section`
   }
 `;
 
+const StyledArticlesWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 24px;
+`;
+
+const StyledArticleImage = styled.img`
+  width: 100%;
+  max-width: 600px;
+`;
+
 const Articles = () => {
   const revealContainer = useRef(null);
   const prefersReducedMotion = usePrefersReducedMotion();
@@ -29,12 +41,12 @@ const Articles = () => {
   return (
     <StyledArticlesSection id="articles" ref={revealContainer}>
       <h2 className="numbered-heading overline">Published Articles</h2>
-      <p align="center">
+      <StyledArticlesWrapper>
         <a
           href="https://medium.com/@scottgrivner/understanding-software-ilities-key-attributes-of-quality-software-d4b7fadc9bbe"
           target="_blank"
           rel="noreferrer">
-          <img
+          <StyledArticleImage
             src="https://github-readme-medium-recent-article.vercel.app/medium/@scottgrivner/0"
             alt="Medium Article 0"
           />
@@ -43,30 +55,30 @@ const Articles = () => {
           href="https://medium.com/@scottgrivner/how-to-secure-api-keys-in-your-projects-87e3de50a314"
           target="_blank"
           rel="noreferrer">
-          <img
+          <StyledArticleImage
             src="https://github-readme-medium-recent-article.vercel.app/medium/@scottgrivner/1"
-            alt="Medium Article 0"
+            alt="Medium Article 1"
           />
         </a>
         <a
           href="https://medium.com/@scottgrivner/effectively-managing-technical-debt-with-todo-fixme-and-other-code-reminders-e0b770f6180a"
           target="_blank"
           rel="noreferrer">
-          <img
+          <StyledArticleImage
             src="https://github-readme-medium-recent-article.vercel.app/medium/@scottgrivner/2"
-            alt="Medium Article 0"
+            alt="Medium Article 2"
           />
         </a>
         <a
           href="https://medium.com/@scottgrivner/how-to-read-xml-and-json-as-a-business-user-4243871bf00f"
           target="_blank"
           rel="noreferrer">
-          <img
+          <StyledArticleImage
             src="https://github-readme-medium-recent-article.vercel.app/medium/@scottgrivner/3"
-            alt="Medium Article 0"
+            alt="Medium Article 3"
           />
         </a>
-      </p>
+      </StyledArticlesWrapper>
     </StyledArticlesSection>
   );
 };
