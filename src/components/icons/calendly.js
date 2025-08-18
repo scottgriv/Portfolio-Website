b7cde20a2a754149f797302ae8f3cde07221b649
Scott Griv <scottgrivner@gmail.com>
2025-08-18 19:41:04 -0400

    Update calendly.js

--- src/components/icons/calendly.js
+++ src/components/icons/calendly.js
@@ -3,8 +3,8 @@ import React from 'react';
 const IconCalendly = () => (
   <svg
     fill="currentColor"
-    width="24"
-    height="24"
+    width="44"
+    height="44"
     viewBox="0 0 32 32"
     xmlns="http://www.w3.org/2000/svg">
     <path
